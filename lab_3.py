af7ec726e36888d0d5d76ec12868d8b8aded9835
kubi <pietrusjakub@gmail.com>
2020-04-03 18:29:17 +0200

    lab_3

---
+++ lab_3.py
@@ -0,0 +1,29 @@
+#treść zadania:
+# Opracuj program, korzystający z wyrażeń regularnych zmień przykładowy nagłówek wiadomości w słownik.
+                                # From: author@example.com
+                                # User-Agent: Thunderbird 1.5.0.9 (X11/20061227)
+                                # MIME-Version: 1.0
+                                # To: editor@example.com
+
+
+import re
+
+data = '''From: author@example.com
+User-Agent: Thunderbird 1.5.0.9 (X11/20061227)
+MIME-Version: 1.0
+To: editor@example.com'''
+
+lines = re.split(r'\n', data)
+temp = []
+
+
+for i in range(len(lines)):
+    temp.append(re.split(r':\s', lines[i]))
+
+dictionary = {}
+
+for i in range(len(temp)):
+    dictionary[temp[i][0]] = temp[i][1]
+
+
+print(dictionary)
